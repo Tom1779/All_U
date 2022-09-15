e1142270e3bcc89c3997788f9169c035ec3b5a1c
FlutLab.io <>
2022-09-15 20:18:01 +0000

    Improved sidebar design

---
+++ lib/page/profile_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text('Profile'),
+          centerTitle: true,
+          backgroundColor: Colors.black,
+        ),
+      );
+}
